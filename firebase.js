4649b6f5cccaff0c98e9b904efc310230b201f07
Sergio Giraldo <sergio@giraldo.com.br>
2024-08-26 14:26:24 +0200

    feat: app check ::by sergio giraldo @ 20240826T1426CEST, gpg signed

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13/firebase-database.js";
-import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.13/firebase-app-check.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAj3A2wkkMSktUeHs5r8SyMDN8aRvUSyW8",
@@ -14,15 +13,8 @@ const firebaseConfig = {
         "https://ibcall2action-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
-const app2 = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// https://www.google.com/recaptcha/admin/site/707755302
-const rProvider = new ReCaptchaV3Provider("6LcmfS8qAAAAAOYgmZA08o_CtG-9Sdm2f9z93ZFc");
-initializeAppCheck(app2, {
-    provider: rProvider
-});
+const db = getDatabase(app);
 
-const db2 = getDatabase(app2);
-
-
-export { db2 };
+export { db };
